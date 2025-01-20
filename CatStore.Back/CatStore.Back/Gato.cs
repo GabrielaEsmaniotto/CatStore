78357e993e71a8e5b9da322b752756efed07ad06
GabrielaEsmaniotto <gabriela.esmaniotto@yahoo.com>
2025-01-20 20:04:26 -0300

    Criação da classe gato

---
+++ CatStore.Back/CatStore.Back/Gato.cs
@@ -0,0 +1,48 @@
+﻿namespace CatStore.Back
+{
+    public class Gato
+    {
+        private string nome;
+        private int idade;
+        private float peso;
+        private string cor;
+
+        public string Miar()
+        {
+            return "Miau";
+        }
+
+        public string GetNome()
+        {
+            return nome;
+        }
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public int GetIdade()
+        {
+            return idade;
+        }
+        public void SetIdade(int idade)
+        {
+            this.idade = idade;
+        }
+        public float GetPeso()
+        {
+            return peso;
+        }
+        public void SetPeso(float peso) 
+        {
+            this.peso = peso;
+        }
+        public string GetCor()
+        {
+            return cor;
+        }
+        public void SetCor(string cor) 
+        {
+            this.cor = cor;   
+        }
+     }
+}
